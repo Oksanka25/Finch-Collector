f620bb29420438316c719a98bd087ba330cae723
Oksanka25 <chekoksa25@gmail.com>
2022-05-26 21:13:09 -0400

    Routes are set up

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+class Home(View):
+    def get(self, request):
+        return HttpResponse("Finch Home")
+
+
+class About(View):
+    def get(self, request):
+        return HttpResponse("Finch About")
+
+
+class Index(View):
+    def get(self, request):
+        return HttpResponse("Finch Index")
