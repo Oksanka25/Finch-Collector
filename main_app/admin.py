d2e189aab4517ad13013e47123e17626e6d17ad8
Oksanka25 <chekoksa25@gmail.com>
2022-05-30 21:47:59 -0400

    Create a super user and and registered the Finch model for Admin Access

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Finch  # import the Artist model from models.py
+# Register your models here.
+
+admin.site.register(Finch)  # this line will add the model to the admin panel
